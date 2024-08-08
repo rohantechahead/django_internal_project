842dc8e3aed60e54e1f417e862a77a647c9da9e1
Prathmesh <prathmeshjain340@gmail.com>
2024-08-08 12:19:40 +0530

    utility comment error resolve

--- utility/authentication_helper.py
+++ utility/authentication_helper.py
@@ -1,12 +1,11 @@
-
-from functools import wraps
-from urllib import request, response
-import jwt
 from datetime import datetime, timedelta
+from functools import wraps
+from urllib import response
+
+import jwt
 from django.conf import settings
-from django.contrib.auth.models import User
+from django.http import JsonResponse
 from rest_framework import status
-from django.http import HttpResponse, JsonResponse
 
 # Secret key for signing the JWT
 SECRET_KEY = settings.SECRET_KEY
@@ -14,6 +13,7 @@ SECRET_KEY = settings.SECRET_KEY
 # Token expiration time
 ACCESS_TOKEN_LIFETIME = timedelta(minutes=5)
 REFRESH_TOKEN_LIFETIME = timedelta(days=1)
+
 
 def generate_access_token(user):
     expiration = datetime.utcnow() + ACCESS_TOKEN_LIFETIME
@@ -24,6 +24,7 @@ def generate_access_token(user):
     }
     return jwt.encode(payload, SECRET_KEY, algorithm='HS256')
 
+
 def generate_refresh_token(user):
     expiration = datetime.utcnow() + REFRESH_TOKEN_LIFETIME
     payload = {
@@ -32,6 +33,7 @@ def generate_refresh_token(user):
         'iat': datetime.utcnow()
     }
     return jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+
 
 def decode_token(token):
     try:
@@ -42,32 +44,28 @@ def decode_token(token):
     except jwt.InvalidTokenError:
         return JsonResponse({"message": "Invalid token"}, status=status.HTTP_403_FORBIDDEN)
 
+
 def is_auth(fun):
     @wraps(fun)
-    def wrap(request,*args, **kwargs):
+    def wrap(request, *args, **kwargs):
         print(request.headers)
-        print(request,*args,**kwargs)
-
+        print(request, *args, **kwargs)
 
         try:
-           token= request.headers.get('Authorization')
-         
-           if not token:
-              return response({"message": "Token is missing!"}, status=status.HTTP_403_FORBIDDEN)
-           
-           decode_token_result = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])#(token)
-    
-           request.decoded_token_result = decode_token_result
-           request.user_id=request.decoded_token_result.get("user_id")
-           return fun(request, *args, **kwargs)
-             
+            token = request.headers.get('Authorization')
+
+            if not token:
+                return response({"message": "Token is missing!"}, status=status.HTTP_403_FORBIDDEN)
+
+            decode_token_result = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])  # (token)
+
+            request.decoded_token_result = decode_token_result
+            request.user_id = request.decoded_token_result.get("user_id")
+            return fun(request, *args, **kwargs)
+
         except jwt.ExpiredSignatureError:
             return JsonResponse({"message": "Token is expired"}, status=status.HTTP_498_INVALID_TOKEN)
         except jwt.InvalidTokenError:
             return JsonResponse({"message": "Invalid token"}, status=status.HTTP_403_FORBIDDEN)
-           
-        
-    
+
     return wrap
-        
-        
