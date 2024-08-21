a7b1f7fe729c170dffea5313a69e96ac5b8dd1cd
Prathmesh <prathmeshjain340@gmail.com>
2024-08-21 11:42:25 +0530

    send-request-api issue resolved

--- user_connection/urls.py
+++ user_connection/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 from user_connection.views import send_request
 
 urlpatterns = [
-    #To be done  # path('demo/', index_page, name='signup')
 path('send-request/', send_request, name='send-request'),
 ]
