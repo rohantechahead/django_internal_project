11869ff67d1377da98d3723dbef0e89d56f5d719
srishti <srishtimishra112@gmail.com>
2024-08-27 14:06:07 +0530

    simple change

--- user_connection/views.py
+++ user_connection/views.py
@@ -3,7 +3,7 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from User_Auth.models import User
-from User_Auth.serializer import LoginSerializer, UserProfileSerializer
+from User_Auth.serializer import LoginSerializer
 from utility.api_documantion_helper import send_request_api_doc, withdraw_send_request_api_doc, accept_reject_api_doc, \
     block_user_api_doc, report_user_api_doc, list_connection_api_doc, search_username_api_doc, get_profile_view_api_doc
 from utility.authentication_helper import is_auth
